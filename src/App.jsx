a1317fb987c9debb2a0bf3f8400e31a034cd7043
Siya Surial <siyasurial34@gmail.com>
2025-10-31 14:28:51 +0530

    Initial commit - Holistica React project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Skincare from "./components/Skincare";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skincare" element={<Skincare />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
