1d807cabd09644f4220a7b1aa28d5b5588bbc4aa
Siya Surial <siyasurial34@gmail.com>
2025-11-18 23:38:58 +0530

    done

--- React_Holistica/src/App.jsx
+++ React_Holistica/src/App.jsx
@@ -3,13 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 import Skincare from "./components/Skincare";
-<<<<<<< HEAD
-import LoginSignup from "./components/LoginSignup";
-
-=======
 import Login from "./components/Login";
 import Signup from "./components/Signup";
->>>>>>> 7bb1716 (done)
 
 function App() {
   return (
@@ -18,18 +13,13 @@ function App() {
         {/* Main Pages */}
         <Route path="/" element={<Home />} />
         <Route path="/skincare" element={<Skincare />} />
-<<<<<<< HEAD
-        <Route path="/login" element={<LoginSignup />} />   {/* Add this */}
-=======
 
         {/* Auth Pages */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
->>>>>>> 7bb1716 (done)
       </Routes>
     </Router>
   );
 }
-
 
 export default App;
